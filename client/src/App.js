f159095071f6889584062995fa8124653ab8954b
Andrew Stewart <1andrewstewart@gmail.com>
2020-11-04 21:34:48 -0500

    starter files

---
+++ client/src/App.js
@@ -0,0 +1,25 @@
+import { useEffect } from "react";
+import logo from "./logo.svg";
+import "./App.css";
+import axios from "axios";
+
+function App() {
+  useEffect(() => {
+    console.log("Make an API call");
+    axios
+      .get("/api/config")
+      .then((response) => {
+        console.log(response);
+      })
+      .catch((err) => {
+        console.log(err);
+      });
+  }, []);
+  return (
+    <div className="App">
+        <h1>Hello world!</h1>
+    </div>
+  );
+}
+
+export default App;
